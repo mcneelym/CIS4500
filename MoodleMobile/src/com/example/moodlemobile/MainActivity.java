4d20dd0e41e930b5cef2a875f6875d53919e0750
Michael McNeely <vbtstudios@gmail.com>
2014-03-20 20:24:22 -0400

    Fixed home having back button

--- MoodleMobile/src/com/example/moodlemobile/MainActivity.java
+++ MoodleMobile/src/com/example/moodlemobile/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
+			getActionBar().setDisplayHomeAsUpEnabled(false);
 		}
 	}
 
