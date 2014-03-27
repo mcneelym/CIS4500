bf5f3092d0f45f4b26dcefefee16dca194e371d4
Michael McNeely <vbtstudios@gmail.com>
2014-03-27 00:06:01 -0400

    added course contents

--- MoodleMobile/src/com/example/moodlemobile/CourseMain.java
+++ MoodleMobile/src/com/example/moodlemobile/CourseMain.java
@@ -2,21 +2,18 @@ package com.example.moodlemobile;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-import android.app.ListActivity;
-import android.content.Context;
+import android.app.ExpandableListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.util.SparseArray;
 
-public class CourseMain extends ListActivity {
+public class CourseMain extends ExpandableListActivity {
 	
 	
 	
@@ -26,27 +23,22 @@ public class CourseMain extends ListActivity {
 		setContentView(R.layout.activity_course_main);
 		new GetCoursesTask(this).execute();
 	}
-
-	@Override 
-    public void onListItemClick(ListView l, View v, int position, long id) {
-
-    }
 	
-	private class GetCoursesTask extends AsyncTask<String, Void, HashMap<String,Long>> {
+	private class GetCoursesTask extends AsyncTask<String, Void,SparseArray<Group>> {
 		
-		private Context mContext;
-	    public GetCoursesTask (Context context){
+		private ExpandableListActivity mContext;
+	    public GetCoursesTask (ExpandableListActivity context){
 	         mContext = context;
 	    }
 		
 		@Override
-		protected HashMap<String,Long> doInBackground(String... info) {
+		protected SparseArray<Group> doInBackground(String... info) {
 			MoodleRestService service = MoodleRestService.getService();
 			if (service != null) {
 				String json;
-				HashMap<String,Long> array = new HashMap<String,Long>();
+				SparseArray<Group> array = new SparseArray<Group>();
 				try {
-					 json = service.getCourses();
+					 json = service.getCourseInformation(service.getCurrentCourseId());
 				} catch (IOException e) {
 					return null;
 				}
@@ -55,7 +47,14 @@ public class CourseMain extends ListActivity {
 					JSONObject json_data = null;
 					for (int i = 0; i < jArray.length(); i++) {
 						json_data = jArray.getJSONObject(i);
-						array.put(json_data.getString("fullname"),json_data.getLong("id"));
+						Group g = new Group(json_data.getString("name"), json_data.getLong("id"), json_data.getInt("visible"));
+						JSONArray childArray = json_data.getJSONArray("modules");
+						for (int j = 0; j < childArray.length(); j++) {
+							JSONObject childData = childArray.getJSONObject(j);
+							GroupChild c = new GroupChild(childData.getString("name"), childData.getString("url"), childData.getString("modicon"), childData.getLong("id"), childData.getInt("visible"));
+							g.children.add(c);
+						}
+						array.append(i, g);
 					}
 				} catch (JSONException e) {
 					return null;
@@ -69,10 +68,10 @@ public class CourseMain extends ListActivity {
 		}
 		
 		@Override
-		protected void onPostExecute(HashMap<String,Long> result) {
+		protected void onPostExecute(SparseArray<Group> result) {
 			if (result != null) {
-				CourseArrayAdapter adapter = new CourseArrayAdapter(mContext, R.layout.row_layout, result);
-				((ListActivity)mContext).setListAdapter(adapter);
+				CoursesContentListAdapter adapter = new CoursesContentListAdapter(mContext, result);
+				((ExpandableListActivity)mContext).setListAdapter(adapter);
 			}
 			else
 			{
@@ -81,25 +80,36 @@ public class CourseMain extends ListActivity {
 		}
 	}
 	
-	private class CourseArrayAdapter extends ArrayAdapter<String> {
-
-		HashMap<String, Long> mIdMap;
+	public class GroupChild {
+		public String name;
+		public String url;
+		public String modIcon;
+		public long id;	
+		public int visible;
 		
-		public CourseArrayAdapter(Context context, int textViewResourceId, HashMap<String,Long> objects) {
-			super(context, textViewResourceId, new ArrayList<String>(objects.keySet()));
-			mIdMap = objects;
-		}
-		
-		@Override
-		public long getItemId(int position) {
-			String item = getItem(position);
-			return mIdMap.get(item);
-		}
-		
-		@Override
-		public boolean hasStableIds() {
-			return true;
+		public GroupChild(String name, String url, String modIcon, long id, int visible) {
+			this.name = name;
+			this.id = id;
+			this.visible = visible;
+			this.url = url;
+			this.modIcon = modIcon;
 		}
 	}
+	
+	public class Group {
+		
+		public String name;
+		public long id;
+		public int visible;
+		public final List<GroupChild> children = new ArrayList<GroupChild>();
+		
+		public Group(String name, long id, int visible) {
+			this.name = name;
+			this.id = id;
+			this.visible = visible;
+		}
+	
+	} 
+	
 
 }
---
+++ MoodleMobile/src/com/example/moodlemobile/CoursesContentListAdapter.java
@@ -0,0 +1,92 @@
+package com.example.moodlemobile;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
+import com.example.moodlemobile.CourseMain.Group;
+import com.example.moodlemobile.CourseMain.GroupChild;
+
+
+public class CoursesContentListAdapter extends BaseExpandableListAdapter {
+	
+	private final SparseArray<Group> groups;
+	public LayoutInflater inflater;
+	public Context activity;
+	
+	public CoursesContentListAdapter(Activity act, SparseArray<Group> groups) {
+		activity = act;
+		this.groups = groups;
+	    inflater = act.getLayoutInflater();
+	}
+	
+	@Override
+	public int getGroupCount() {
+		return groups.size();
+	}
+
+	@Override
+	public int getChildrenCount(int groupPosition) {
+		return groups.get(groupPosition).children.size();
+	}
+
+	@Override
+	public Object getGroup(int groupPosition) {
+		return groups.get(groupPosition);
+	}
+
+	@Override
+	public Object getChild(int groupPosition, int childPosition) {
+		return groups.get(groupPosition).children.get(childPosition);
+	}
+
+	@Override
+	public long getGroupId(int groupPosition) {
+		return 0;
+	}
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return 0;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.listrow_group, null);
+		}
+		Group group = (Group) getGroup(groupPosition);
+		((CheckedTextView) convertView).setText(group.name);
+		((CheckedTextView) convertView).setChecked(isExpanded);
+		return convertView;
+	}
+
+	@Override
+	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+		final String children = ((GroupChild)getChild(groupPosition, childPosition)).name;
+		TextView text = null;
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.listrow_details, null);
+		}
+		text = (TextView) convertView.findViewById(R.id.textView1);
+		text.
+		text.setText(children);
+		return convertView;
+	}
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
+	}
+
+}
